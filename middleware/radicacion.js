656e2b5297c9073b798638119083c78507f5d455
AGUSTIN DI NARDO <agustin.dinardo@vicentelopez.gov.ar>
2023-01-16 12:28:45 -0300

    undo

--- middleware/radicacion.js
+++ middleware/radicacion.js
@@ -9,38 +9,37 @@ module.exports = (req, res, next) => {
 
   pythonProcess.stdout.on("data", async (data) => {
     const decodedData = data.toString();
-    console.log(decodedData);
 
-    // const _data = JSON.parse(decodedData.replace(/'/g, '"'));
+    const _data = JSON.parse(decodedData.replace(/'/g, '"'));
 
-    // const { provincia, localidad } = _data;
+    const { provincia, localidad } = _data;
 
-    // if (provincia !== "BUENOS AIRES" && provincia !== "CAPITAL FEDERAL") {
-    //   const res = await pool.query(
-    //     "select id_barrio from barrios where barrio=$1",
-    //     [provincia]
-    //   );
-    //   const [{ id_barrio }] = res.rows;
-    //   req.body.localidadInfractor = id_barrio;
-    // } else {
-    //   if (localidad === "CAPITAL FEDERAL") {
-    //     const id_barrio = 51;
-    //     req.body.localidadInfractor = id_barrio;
-    //   } else {
-    //     const res = await pool.query(
-    //       "select id_barrio from barrios where barrio=$1",
-    //       [localidad]
-    //     );
-    //     if (res.rowCount === 0) {
-    //       const id_barrio = 44;
-    //       req.body.localidadInfractor = id_barrio;
-    //     } else {
-    //       const [{ id_barrio }] = res.rows;
-    //       req.body.localidadInfractor = id_barrio;
-    //     }
-    //   }
-    // }
-    // data = null;
+    if (provincia !== "BUENOS AIRES" && provincia !== "CAPITAL FEDERAL") {
+      const res = await pool.query(
+        "select id_barrio from barrios where barrio=$1",
+        [provincia]
+      );
+      const [{ id_barrio }] = res.rows;
+      req.body.localidadInfractor = id_barrio;
+    } else {
+      if (localidad === "CAPITAL FEDERAL") {
+        const id_barrio = 51;
+        req.body.localidadInfractor = id_barrio;
+      } else {
+        const res = await pool.query(
+          "select id_barrio from barrios where barrio=$1",
+          [localidad]
+        );
+        if (res.rowCount === 0) {
+          const id_barrio = 44;
+          req.body.localidadInfractor = id_barrio;
+        } else {
+          const [{ id_barrio }] = res.rows;
+          req.body.localidadInfractor = id_barrio;
+        }
+      }
+    }
+    data = null;
     next();
   });
 
