c161e255eeab2c94dcbe534cec4f833f9432277b
AGUSTIN DI NARDO <agustin.dinardo@vicentelopez.gov.ar>
2023-02-27 11:20:43 -0300

    cache

--- server.js
+++ server.js
@@ -12,6 +12,18 @@ app.use(
   })
 );
 
+const setCache = (req, res, next) => {
+  const period = 60 * 5;
+
+  if (req.method == "GET") {
+    res.set("Cache-control", `public, max-age=${period}`);
+  } else {
+    res.set("Cache-control", `no-store`);
+  }
+  next();
+};
+app.use(setCache);
+
 app.use("/", require("./routes"));
 
 app.listen(port);
